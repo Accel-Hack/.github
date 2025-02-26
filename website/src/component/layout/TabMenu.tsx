c5c3c7c5b0f7fde80d3f75dfcb5bdf8f89be54bc
kyosuke <kyosuke.k.94@gmail.com>
2025-02-26 12:27:04 +0900

    make tab menu

--- website/src/component/layout/TabMenu.tsx
+++ website/src/component/layout/TabMenu.tsx
@@ -1,15 +1,33 @@
+import React from 'react';
 import { Page } from '@/enum/Page';
 import styles from './TabMenu.module.css';
+import TabItem from '@/component/molecule/TabItem';
+
+const MainTabs = [Page.ABOUT, Page.SERVICE, Page.RECRUIT, Page.BLOG];
+
+const SubTabs = [Page.CONTACT, Page.PRIVACY_POLICY];
 
 const TabMenu: React.FC = () => {
   return (
     <div className={styles.container}>
-      <a href={Page.ABOUT}>ABOUT</a> <br />
-      <a href={Page.SERVICE}>SERVICE</a> <br />
-      <a href={Page.RECRUIT}>RECRUIT</a> <br />
-      <a href={Page.BLOG}>BLOG</a> <br />
-      <a href={Page.CONTACT}>CONTACT</a> <br />
-      <a href={Page.PRIVACY_POLICY}>PRIVACY_POLICY</a>
+      <div className={styles.main_tabs}>
+        {MainTabs.map((page, i) => {
+          return (
+            <div key={i} className={styles.tab}>
+              <TabItem type="primary" page={page} />
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.sub_tabs}>
+        {SubTabs.map((page, i) => {
+          return (
+            <div key={i} className={styles.tab}>
+              <TabItem type="secondary" key={i} page={page} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
