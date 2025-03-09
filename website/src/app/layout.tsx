dc88c4bace81f2d15fa96d18c8e5ffd3db074c88
kyosuke <kyosuke.k.94@gmail.com>
2025-03-09 18:01:20 +0900

    fix responsive layout

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -5,74 +5,101 @@ import Header from '@/component/layout/Header';
 import Footer from '@/component/layout/Footer';
 import './globals.css';
 import styles from './layout.module.css';
-import Border from '@/asset/img/layout/border.png';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
-  subsets: ['latin']
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
   title: 'AccelHack',
-  description: 'Website of AccelHack.'
+  description: 'Website of AccelHack.',
 };
 
 export const viewport: Viewport = {
   width: 'device-width',
-  initialScale: 1
+  initialScale: 1,
 };
 
 export default function RootLayout({
-                                     children
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-    <body
-      className={`${styles.body} ${geistSans.variable}`}
-    >
-    {/*<main className={styles.main}>*/}
-    {/*  <div className={styles.main_layout}>*/}
-    {/*    <div*/}
-    {/*      className={styles.border}*/}
-    {/*      style={{ backgroundImage: `url(${Border.src})` }}*/}
-    {/*    />*/}
-    {/*    <div className={styles.content}>{children}</div>*/}
-    {/*  </div>*/}
-    {/*</main>*/}
-    {/*<footer className={styles.footer}>*/}
-    {/*  <Footer />*/}
-    {/*</footer>*/}
-    <>
-      <header className={styles.header}>
-        <Header />
-      </header>
-      <main className={styles.main}>
-
-        メインコンテンツ
-      </main>
-      <footer className={styles.footer}>
-        <Footer />
-      </footer>
-    </>
-    {/*左右のAccelHackのレイヤー*/}
-    <>
-      <div className={styles.background}>
-        <div
-          className={styles.background_logo}
-        >
-          左背景<br />（幅固定）
-        </div>
-        <div
-          className={styles.background_logo}
-        >
-          右背景<br />（幅固定）
-        </div>
-      </div>
-    </>
-    </body>
+      <body className={`${styles.body} ${geistSans.variable}`}>
+        <>
+          <header className={styles.header}>
+            <Header />
+          </header>
+          <main className={styles.main}>
+            <div className={styles.content}>{children}</div>
+            {/* メインページの枠 */}
+            <>
+              <div className={styles.frame}>
+                <div
+                  className={`${styles.frame_column} ${styles.frame_column_top}`}
+                >
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_left}`}
+                  />
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_middle}`}
+                  />
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_right}`}
+                  />
+                </div>
+                <div
+                  className={`${styles.frame_column} ${styles.frame_column_middle}`}
+                >
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_left}`}
+                  />
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_middle}`}
+                  />
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_right}`}
+                  />
+                </div>
+                <div
+                  className={`${styles.frame_column} ${styles.frame_column_bottom}`}
+                >
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_left}`}
+                  />
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_middle}`}
+                  />
+                  <div
+                    className={`${styles.frame_row} ${styles.frame_row_right}`}
+                  />
+                </div>
+              </div>
+            </>
+          </main>
+          <footer className={styles.footer}>
+            <Footer />
+          </footer>
+        </>
+        {/* 左右のAccelHackのレイヤー */}
+        <>
+          <div className={styles.background}>
+            <div className={styles.background_logo}>
+              左背景
+              <br />
+              （幅固定）
+            </div>
+            <div className={styles.background_logo}>
+              右背景
+              <br />
+              （幅固定）
+            </div>
+          </div>
+        </>
+      </body>
     </html>
-  )
-    ;
+  );
 }
