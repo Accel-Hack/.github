c8711348bcf38baffad46fd486a64a06b6c6d1ec
Kengo Watanabe <kengo022348@gmail.com>
2025-03-26 16:24:28 +0900

    いったんtop終わり、残タスクはある

--- website/src/component/organism/NewsSummary.tsx
+++ website/src/component/organism/NewsSummary.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewsOneLine from '@/component/molecule/NewsOneLine';
-
+import Cursor from '@/asset/img/common/cursor.svg';
+import styles from './NewsSummary.module.css';
 type Props = {
   newses: {
     date: string;
@@ -10,11 +11,12 @@ type Props = {
 
 const NewsSummary: React.FC<Props> = ({ newses }: Props) => {
   return (
-    <div className={'px-4 py-2 bg-white border-t-[0.5px] border-b-[0.5px] border-solid border-border'}>
-      <span className={'text-xl block mb-2'}>News</span>
+    <div className={styles.news_summary_container}>
+      <span className={styles.news_summary_title}>News</span>
       {newses.map((news, i) => {
         return <NewsOneLine key={i} news={news} />;
       })}
+      <Cursor className={styles.cursor} />
     </div>
   );
 };
