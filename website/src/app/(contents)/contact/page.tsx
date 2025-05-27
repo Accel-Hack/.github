3c8a8a8d5c6173032252d03f413568d2686ef0f0
kyosuke <kyosuke.k.94@gmail.com>
2025-05-28 07:59:31 +0900

    fix lint

--- website/src/app/(contents)/contact/page.tsx
+++ website/src/app/(contents)/contact/page.tsx
@@ -3,7 +3,9 @@ import { redirect } from 'next/navigation';
 
 const ContactPage: React.FC = () => {
   // redirect google form
-  return redirect('https://docs.google.com/forms/d/e/1FAIpQLSdSTDPgMCzw7rfAHZYNcFkWGfUCLMzpeeUzxpad_9Pg_TVB1A/viewform');
+  return redirect(
+    'https://docs.google.com/forms/d/e/1FAIpQLSdSTDPgMCzw7rfAHZYNcFkWGfUCLMzpeeUzxpad_9Pg_TVB1A/viewform',
+  );
 };
 
 export default ContactPage;
