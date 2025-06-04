177dea78536560a168c95de592746e8455c3d6b2
kyosuke <kyosuke.k.94@gmail.com>
2025-06-04 13:26:10 +0900

    fix deploy action

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from 'next';
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+const repo = '.github'; // GitHubのリポジトリ名
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
