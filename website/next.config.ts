3105509ddff7c5ca77476b2778fbe81963bff274
kyosuke <kyosuke.k.94@gmail.com>
2025-06-04 14:19:13 +0900

    fix image path2

--- website/next.config.ts
+++ website/next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: '/',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
