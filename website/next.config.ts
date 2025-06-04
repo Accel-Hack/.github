58a30587b55cdf119164ceddef002f2943d301f5
kyosuke <kyosuke.k.94@gmail.com>
2025-06-04 14:09:43 +0900

    fix image path

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '',
-  assetPrefix: '/',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
