ffdd219e8ad258595375926d27b97802a57fa5d5
kyosuke <kyosuke.k.94@gmail.com>
2025-06-04 14:01:29 +0900

    add base path

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '',
+  assetPrefix: '/',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
